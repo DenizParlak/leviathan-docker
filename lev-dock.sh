448579dd5dfa46cfc7d6861c993a51d45796555f
DenizParlak <denizparlak@protonmail.ch>
2017-04-28 01:51:55 +0300

    Update lev-dock.sh

--- lev-dock.sh
+++ lev-dock.sh
@@ -4,7 +4,7 @@
 
 This script creates the Docker environment required for the Leviathan framework to work in Red Hat and CentOS variants.
 
-denizparlak
+deniz
 
 '
 
@@ -15,15 +15,15 @@ systemctl enable docker
 
 docker pull docker.io/kalilinux/kali-linux-docker
 
-docker run -i -t docker.io/kalilinux/kali-linux-docker /bin/bash
+docker run -i -d -t docker.io/kalilinux/kali-linux-docker /bin/bash
 
 id="$(docker ps | awk {'print $1'} | grep -v CON*)"
 
-docker exec $id apt-get update &&  python python-pip git gcc make libpcap-dev build-essential checkinstall libssl.0-dev libssh-dev libffi-dev python-dev
+docker exec $id bash -c 'apt-get update ; apt-get install -y python python-pip git gcc make libpcap-dev build-essential checkinstall libssh-dev libffi-dev python-dev'
 
-docker exec $id git clone https://github.com/leviathan-framework/leviathan.git
+docker exec $id bash -c 'git clone https://github.com/leviathan-framework/leviathan.git'
 
-docker exec $id cd leviathan
+docker exec $id bash -c 'cd leviathan'
 
-docker exec $id pip install -r requirements.txt
+docker exec $id bash -c 'pip install -r requirements.txt'
 
